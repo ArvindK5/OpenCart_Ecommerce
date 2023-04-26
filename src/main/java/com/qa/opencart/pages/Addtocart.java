909cec7001fd3d9b51c07def8e44c9987082d4ce
Arvind kumar <arvindkumar9057@gmail.com>
2023-04-26 18:37:02 +0530

    adde locator and page actions

--- src/main/java/com/qa/opencart/pages/Addtocart.java
+++ src/main/java/com/qa/opencart/pages/Addtocart.java
@@ -1,5 +1,20 @@
 package com.qa.opencart.pages;
 
-public class Addtocart {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class Addtocart {
+	
+	WebDriver driver;
+	
+	public Addtocart() {
+		this.driver = driver;
+		
+	}
+   private  By cart = By.id("cart");
+   
+   
+   public void addtoCart() {
+	   
+   }
 }
